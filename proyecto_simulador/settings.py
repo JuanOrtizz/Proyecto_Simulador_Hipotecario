b06eca23a81b815217d76810c59ad146cb2a4a3a
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-06-19 15:59:12 -0300

    fix: soluciono conflictos
    # Conflicts:
    #	render.yaml
    #	requirements.txt

--- proyecto_simulador/settings.py
+++ proyecto_simulador/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os.path
+import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 
@@ -81,14 +82,11 @@ WSGI_APPLICATION = 'proyecto_simulador.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
@@ -135,3 +133,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG: # Para cuando este en produccion para mejorar la seguridad
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
