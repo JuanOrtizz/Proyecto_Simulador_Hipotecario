abb92b28a560e3533ad7ea169e4192872ba2f39f
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-06-19 20:52:44 -0300

    fix: Agrego User al archivo admin.py

--- simulador/admin.py
+++ simulador/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 # myapp/admin.py
 from django.contrib import admin
 from .models import Consultas, Solicitudes
+from django.contrib.auth.models import User
 
-admin.site.register([Consultas, Solicitudes])
+admin.site.register([Consultas, Solicitudes, User])
