6d1507a8e039144c4d54d0a04fdcbddac27b5275
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-06-19 14:06:55 -0300

    refactor: Borro imports innecesarios en archivos 'urls.py' y 'views.py'

--- simulador/urls.py
+++ simulador/urls.py
@@ -1,10 +1,8 @@
-from tkinter.font import names
-
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
-from .views import SolicitudesViewSet, modificar_solicitud, eliminar_solicitud, eliminar_consulta, ConsultasViewSet
+from .views import SolicitudesViewSet, ConsultasViewSet
 
 #declaro el router y los endpoints
 router = DefaultRouter()
@@ -18,9 +16,9 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name= 'dashboard'), #path para el dashboard
     path('dashboard/solicitudes/', views.pagina_solicitudes, name= 'solicitudes'), #path para el el panel de solicitudes
     path('dashboard/consultas/', views.pagina_consultas, name= 'consultas'), #path para el panel de consultas
-    path('modificarSolicitud/<int:solicitud_id>/', modificar_solicitud, name='modificar_solicitud'),# path para modificar solicitudes
-    path('eliminarSolicitud/<int:solicitud_id>/', eliminar_solicitud, name='eliminar_solicitud'),# path para eliminar solicitudes
-    path('eliminarConsulta/<int:consulta_id>/', eliminar_consulta, name='eliminar_consulta'),# path para eliminar consultas
+    path('modificarSolicitud/<int:solicitud_id>/', views.modificar_solicitud, name='modificar_solicitud'),# path para modificar solicitudes
+    path('eliminarSolicitud/<int:solicitud_id>/', views.eliminar_solicitud, name='eliminar_solicitud'),# path para eliminar solicitudes
+    path('eliminarConsulta/<int:consulta_id>/', views.eliminar_consulta, name='eliminar_consulta'),# path para eliminar consultas
     path('login/', auth_views.LoginView.as_view(template_name='simulador/login.html'), name='login'), #path para el login
     path('logout/', auth_views.LogoutView.as_view(), name='logout'), #path para el logout
     path('api/', include(router.urls)), #path para exponer los endpoints con el router
