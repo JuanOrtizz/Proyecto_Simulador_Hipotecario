dce8117b0b11b8a7884b57744e624eef69d062e7
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-03-13 03:00:32 -0300

    feat: Elimino configuraciones de envio de email SMTP (settings.py)

--- proyecto_simulador/settings.py
+++ proyecto_simulador/settings.py
@@ -14,15 +14,6 @@ import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 
-# configuro el servidor smtp
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
